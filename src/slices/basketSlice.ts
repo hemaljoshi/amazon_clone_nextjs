1169e3ceaabd23e957fb38d197ff67bcc4ae2f88
joshihemal <hemaljoshi738@gmail.com>
2023-11-03 08:55:10 +0530

    project_setup

--- src/slices/basketSlice.ts
+++ src/slices/basketSlice.ts
@@ -16,6 +16,6 @@ export const basketSlice = createSlice({
 export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
 // Selectors - This is how we pull information from the Global store slice
-export const selectItems = (state) => state.basket.items;
+export const selectItems = (state: any) => state.basket.items;
 
 export default basketSlice.reducer;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        amazon_blue: {
+          light: "#232F3E",
+          DEFAULT: "#131921",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 }
 export default config
